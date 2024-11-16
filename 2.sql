32ea5c430615e2f369a2ab5179abe9ce96c681df
Thomas-Black <150971708+Black-Simon45@users.noreply.github.com>
2024-11-16 10:03:39 -0300

    Add files via upload

---
+++ 2.sql
@@ -0,0 +1,21 @@
+create database feriados;
+use feriado;
+
+create table alunos(
+nome varchar(255),
+idade int,
+cidade varchar(255)
+);
+
+insert into alunos  values ("Aluno G", 30, "Amazonas");
+
+create database loja_online2;
+use loja_online2;
+
+create table
+
+
+
+
+
+
